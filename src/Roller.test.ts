39c3fbbabb92bb3dc81b5f9da9124067f8f4bd35
Anoushka Sharma <35841938+Anoushka444@users.noreply.github.com>
2023-03-28 10:43:43 -0700

    Update Roller.test.ts

--- src/Roller.test.ts
+++ src/Roller.test.ts
@@ -1,12 +1,14 @@
-import {Roller} from './Roller';
+import { Roller } from "./Roller";
 
-describe("Smoke test", ()=> {
-  test("The test scaffold runs successfully.", ()=> {
+describe("Smoke test", () => {
+  test("The test scaffold runs successfully.", () => {
     expect(true).toBe(true);
   });
-})
+});
 
-describe("Roller tests", ()=> {
-  test("Description", () => {
+describe("Roller tests", () => {
+  test("Description should return a string with the number of faces.", () => {
+    const roller = new Roller(6);
+    expect(roller.getDescription()).toBe("This roller has 6 faces.");
   });
 });
