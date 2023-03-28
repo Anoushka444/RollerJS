4b0ee400214029cd12ff9e16e882529dd5c5a6d8
Anoushka Sharma <35841938+Anoushka444@users.noreply.github.com>
2023-03-28 11:39:57 -0700

    Updated Roller.test.ts with new tests for roller.ts

--- src/Roller.test.ts
+++ src/Roller.test.ts
@@ -1,36 +1,61 @@
-export class Roller {
-    private _distribution: Map<number, number>;
-    private _last: number;
-    private _faces: number;
+import { Roller } from "./Roller";
 
-    constructor(faces: number) {
-        this._faces = faces >= 2 ? faces : 6;
-        this._distribution = new Map<number, number>();
-        for (let i = 1; i <= this._faces; i++) {
-            this._distribution.set(i, 0);
-        }
-        this._last = 0;
-    }
+describe("Smoke test", () => {
+    test("The test scaffold runs successfully.", () => {
+        expect(true).toBe(true);
+    });
+});
 
-    roll(value: number): number {
-        if (value >= 1 && value <= this._faces) {
-            this._last = value;
-            this._distribution.set(value, this._distribution.get(value)! + 1);
-        } else {
-            value = 0;
-        }
-        return value;
-    }
 
-    last(): number {
-        return this._last;
-    }
+describe("Roller tests", () => {
+    test("Roll a valid value should update the last roll and distribution.", () => {
+        const roller = new Roller(6);
+        roller.roll(3);
+        expect(roller.last()).toBe(3);
+        expect(roller.distribution().get(3)).toBe(1);
+    });
 
-    distribution(): Map<number, number> {
-        return new Map(this._distribution);
-    }
+    test("Roll an invalid value should return 0 and not update the last roll or distribution.", () => {
+        const roller = new Roller(6);
+        roller.roll(7);
+        expect(roller.last()).toBe(0);
+        expect(roller.distribution().get(7)).toBeUndefined();
+    });
 
-    getDescription(): string {
-        return `This roller has ${this._faces} faces.`;
-    }
-}
+    test("Roll a valid value should return the same value.", () => {
+        const roller = new Roller(6);
+        const result = roller.roll(3);
+        expect(result).toBe(3);
+    });
+
+    test("Roll an invalid value should return 0.", () => {
+        const roller = new Roller(6);
+        const result = roller.roll(7);
+        expect(result).toBe(0);
+    });
+
+    test("Roll multiple valid values should update the distribution for each face that is rolled.", () => {
+        const roller = new Roller(6);
+        roller.roll(3);
+        roller.roll(4);
+        roller.roll(6);
+        roller.roll(3);
+        expect(roller.distribution().get(1)).toBe(0);
+        expect(roller.distribution().get(2)).toBe(0);
+        expect(roller.distribution().get(3)).toBe(2);
+        expect(roller.distribution().get(4)).toBe(1);
+        expect(roller.distribution().get(5)).toBe(0);
+        expect(roller.distribution().get(6)).toBe(1);
+    });
+
+    test("Roll multiple invalid values should not update the distribution for any face and should return 0 for each roll.", () => {
+        const roller = new Roller(6);
+        roller.roll(7);
+        roller.roll(0);
+        roller.roll(-1);
+        expect(roller.distribution().get(7)).toBeUndefined();
+        expect(roller.distribution().get(0)).toBeUndefined();
+        expect(roller.distribution().get(-1)).toBeUndefined();
+        expect(roller.last()).toBe(0);
+    });
+});
