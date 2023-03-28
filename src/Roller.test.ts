e05446cc57784758faba9ffed79aa1a1b834973a
Anoushka Sharma <35841938+Anoushka444@users.noreply.github.com>
2023-03-28 10:43:59 -0700

    Update Roller.test.ts

--- src/Roller.test.ts
+++ src/Roller.test.ts
@@ -1,14 +1,36 @@
-import { Roller } from "./Roller";
+export class Roller {
+    private _distribution: Map<number, number>;
+    private _last: number;
+    private _faces: number;
 
-describe("Smoke test", () => {
-  test("The test scaffold runs successfully.", () => {
-    expect(true).toBe(true);
-  });
-});
+    constructor(faces: number) {
+        this._faces = faces >= 2 ? faces : 6;
+        this._distribution = new Map<number, number>();
+        for (let i = 1; i <= this._faces; i++) {
+            this._distribution.set(i, 0);
+        }
+        this._last = 0;
+    }
 
-describe("Roller tests", () => {
-  test("Description should return a string with the number of faces.", () => {
-    const roller = new Roller(6);
-    expect(roller.getDescription()).toBe("This roller has 6 faces.");
-  });
-});
+    roll(value: number): number {
+        if (value >= 1 && value <= this._faces) {
+            this._last = value;
+            this._distribution.set(value, this._distribution.get(value)! + 1);
+        } else {
+            value = 0;
+        }
+        return value;
+    }
+
+    last(): number {
+        return this._last;
+    }
+
+    distribution(): Map<number, number> {
+        return new Map(this._distribution);
+    }
+
+    getDescription(): string {
+        return `This roller has ${this._faces} faces.`;
+    }
+}
